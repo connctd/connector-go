b897c089687081a4408d1b18c2a5e2c825f23255
Sebastian Garn <sebastian.garn@posteo.de>
2020-10-07 13:07:30 +0200

    Naming parts of payload-to-sign; use constants to avoid modification by malicious libs; make vars private

--- validation.go
+++ validation.go
@@ -9,55 +9,76 @@ import (
 )
 
 // SignedHeaderKey defines a header field name
-type SignedHeaderKey string
+type signedHeaderKey string
 
-var (
-	// SignatureFragmentDelimiter defines how different fragments like headers and
+const (
+	// signatureFragmentDelimiter defines how different fragments like headers and
 	// body are concatenated to a signable payload. CRLF is already used as a
 	// separator for http 1.1 headers and payloads (https://tools.ietf.org/html/rfc7230#page-19)
 	// which means underlying libraries should already be aware of correct
 	// CRLF handling (e.g. prevent CRLF injection)
-	SignatureFragmentDelimiter = "\r\n"
+	signatureFragmentDelimiter = "\r\n"
+
+	keyValueSeparator = ":"
 
 	// SignedHeaderKeyDate stands for the date header
-	SignedHeaderKeyDate = SignedHeaderKey("Date")
+	signedHeaderKeyDate = signedHeaderKey("Date")
 
 	// SignedHeaderKeys defines a list of headers that are used to build
 	// the payload-to-be-signed. If a request does not contain all of these
 	// headers it can't be signed nor validated and thus is invalid.
 	// The order of keys inside this list defines how the payload-to-be-signed
 	// is constructed
-	SignedHeaderKeys = []SignedHeaderKey{
-		SignedHeaderKeyDate,
-	}
+)
 
-	// ErrorMissingHeader thrown if one of the signed headers is missing
-	ErrorMissingHeader = errors.New("A required header is missing")
+var (
+	signedHeaderKeys = []signedHeaderKey{
+		signedHeaderKeyDate,
+	}
 )
 
 // SignablePayload builds the payload which can be signed
 // Method\r\nHost\r\nRequestURI\r\nDate Header Value\r\nBody
-// Example: GET\r\nfoo.com:8080\r\n/bar?hello=world\r\nWed, 07 Oct 2020 10:00:00 GMT\r\n{\"hello\":\"world\"}
+// Example: (method):GET\r\n(url):https://foo.com:8080/bar?hello=world\r\n(Date):Wed, 07 Oct 2020 10:00:00 GMT\r\n(body):{\"hello\":\"world\"}
 func SignablePayload(method string, host string, url *url.URL, headers http.Header, body []byte) ([]byte, error) {
 	var b bytes.Buffer
 
+	// write method
+	b.WriteString("(method)")
+	b.WriteString(keyValueSeparator)
 	b.WriteString(method)
-	b.WriteString(SignatureFragmentDelimiter)
-	b.WriteString(host)
-	b.WriteString(SignatureFragmentDelimiter)
-	b.WriteString(url.RequestURI())
-	b.WriteString(SignatureFragmentDelimiter)
+	b.WriteString(signatureFragmentDelimiter)
 
-	for _, currHeader := range SignedHeaderKeys {
+	// write url
+	b.WriteString("(url)")
+	b.WriteString(keyValueSeparator)
+	b.WriteString(url.String())
+	b.WriteString(signatureFragmentDelimiter)
+
+	// write all required headers
+	for _, currHeader := range signedHeaderKeys {
 		value := headers.Get(string(currHeader))
 		if value == "" {
 			fmt.Println(currHeader)
 			return []byte{}, ErrorMissingHeader
 		}
+
+		b.WriteString("(" + string(currHeader) + ")")
+		b.WriteString(keyValueSeparator)
 		b.WriteString(value)
-		b.WriteString(SignatureFragmentDelimiter)
+		b.WriteString(signatureFragmentDelimiter)
 	}
 
+	// write body
+	b.WriteString("(body)")
+	b.WriteString(keyValueSeparator)
 	b.Write(body)
+	b.WriteString(signatureFragmentDelimiter)
+
 	return b.Bytes(), nil
 }
+
+var (
+	// ErrorMissingHeader thrown if one of the signed headers is missing
+	ErrorMissingHeader = errors.New("A required header is missing")
+)
--- validation_test.go
+++ validation_test.go
@@ -20,7 +20,7 @@ func TestSignatureComposition(t *testing.T) {
 		t.Fatalf("Failed to marshal body: %v", err)
 	}
 
-	req, err := http.NewRequest(http.MethodGet, "http://foo.com:8080/bar?hello=world", bytes.NewReader(body))
+	req, err := http.NewRequest(http.MethodGet, "https://foo.com:8080/bar?hello=world", bytes.NewReader(body))
 	if err != nil {
 		t.Fatalf("Failed to create new request: %v", err)
 	}
@@ -33,7 +33,28 @@ func TestSignatureComposition(t *testing.T) {
 		t.Fatalf("Failed to create signable payload: %v", err)
 	}
 
-	expected := "GET\r\nfoo.com:8080\r\n/bar?hello=world\r\nWed, 07 Oct 2020 10:00:00 GMT\r\n{\"hello\":\"world\"}"
+	expected := "(method):GET\r\n(url):https://foo.com:8080/bar?hello=world\r\n(Date):Wed, 07 Oct 2020 10:00:00 GMT\r\n(body):{\"hello\":\"world\"}\r\n"
+
+	if string(toBeSigned) != expected {
+		t.Fatalf("Generated payload and expected payload do not match.\nExpctd: %v\n Given: %v", expected, string(toBeSigned))
+	}
+}
+
+func TestSignatureCompositionWithEmptyBody(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "https://foo.com:8080/bar?hello=world", nil)
+	if err != nil {
+		t.Fatalf("Failed to create new request: %v", err)
+	}
+
+	fakeTime := time.Date(2020, 10, 7, 10, 0, 0, 0, time.Now().UTC().Location())
+	req.Header.Set("Date", fakeTime.Format(http.TimeFormat))
+
+	toBeSigned, err := SignablePayload(req.Method, req.Host, req.URL, req.Header, nil)
+	if err != nil {
+		t.Fatalf("Failed to create signable payload: %v", err)
+	}
+
+	expected := "(method):GET\r\n(url):https://foo.com:8080/bar?hello=world\r\n(Date):Wed, 07 Oct 2020 10:00:00 GMT\r\n(body):\r\n"
 
 	if string(toBeSigned) != expected {
 		t.Fatalf("Generated payload and expected payload do not match.\nExpctd: %v\n Given: %v", expected, string(toBeSigned))
