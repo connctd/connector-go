ca4d18748be43d7f679693fcbdc76fe63e8e1d12
Steffen Heidel <kontakt@sthh.eu>
2022-01-20 14:25:42 +0100

    Fixed db query

--- db/default_database.go
+++ db/default_database.go
@@ -52,7 +52,7 @@ var (
 	statementGetInstanceByID              = `SELECT id, token, installation_id FROM instances WHERE id = ?`
 	statementGetInstanceByThingID         = `SELECT id, token, installation_id FROM instances, (SELECT instance_id FROM instance_thing_mapping WHERE thing_id = ? LIMIT 1) mapping WHERE id = instance_id;`
 	statementGetInstances                 = `SELECT id, token, installation_id FROM instances`
-	statementInsertInstanceConfig         = `INSERT INTO instance_configuration (installation_id, id, value) VALUES (?, ?, ?)`
+	statementInsertInstanceConfig         = `INSERT INTO instance_configuration (instance_id, id, value) VALUES (?, ?, ?)`
 	statementGetConfigurationByInstanceID = `SELECT id, value FROM instance_configuration WHERE instance_id = ?`
 	statementGetThingsByInstanceID        = `SELECT thing_id FROM instance_thing_mapping WHERE instance_id = ?`
 	statementRemoveInstanceById           = `DELETE FROM instances WHERE id = ?`
